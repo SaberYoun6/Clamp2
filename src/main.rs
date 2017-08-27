787d02bba230332e6476d0e7cc3ab753af4c9280
yuriprym <samuel.young.103@gmail.com>
2017-08-27 19:13:19 -0400

    so i add in partialord + other things

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
  // I need clamp to continue 
 #[cfg(Debug)]
 fn clamp<T : PartialOrd + PartialEq > ( v : T, lo : T, hi : T) -> T {   
-    assert!(hi != lo,"{:?}= hi, {:?}=low",hi,lo);
+    assert!(hi != lo,"{:?} = hi, {:?} = low",hi,lo);
     if v < lo {
         return  lo; 
-     } else if hi < v{
+     } else if hi < v {
         return  hi;
     } else {
         return v;
