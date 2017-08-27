61db3334b205097d6f406f3c5482129662e9f0ff
yuriprym <samuel.young.103@gmail.com>
2017-08-27 03:02:24 -0400

    so appearantly when testing I'm still having one error and it's any unresolved names

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,18 @@
  // someone started this project and didn't put any work into to it 
  // this will also be the first cargo that i will actaul upload to the cargo page 
  // I need clamp to continue 
-extern crate compare;
-use compare::{Compare, natural };
-use std::cmp::Ordering;
-use std::cmp::PartialOrd;
-use std::iter::Iterator;
-use 
-struct Clamp<T: Copy>{
-    v:T,
-    lo:T,
-    hi:T,
-}
-impl <T> Clamp<T>{
-    fn clamp( v : T, lo : T, hi : T) -> T 
-    where T: Float 
-    {   
-        let cmp2 = T::zero();
-        let cmp = assert!(hi != lo,"{:?}= hi, {:?}=low",hi,lo);
-        if v < lo
-        {
-            cmp2 = lo; 
-        } else if hi < v
-        {
-            cmp2=  hi;
-        } else 
-        {
-            cmp2 = v;
-        }
-        let ret = cmp.cmp2;
-        return ret;
+#[cfg(Debug)]
+fn clamp<T : PartialOrd + PartialEq > ( v : T, lo : T, hi : T) -> T {   
+    assert!(hi != lo,"{:?}= hi, {:?}=low",hi,lo);
+    if v < lo {
+        return  lo; 
+     } else if hi < v{
+        return  hi;
+    } else {
+        return v;
     }
 }
+
 fn main () {
-    let x = Clamp( v: 1f64, lo:2f64, hi:1f64);
-    println!("{}", x.clamp());
+    println!("{:?}", clamp(4.89,0.0,8.9342));
 }
