4e4a9dbd833b1a4798b899b6c889753f4e4b7f91
yuriprym <samuel.young.103@gmail.com>
2017-08-27 19:31:56 -0400

    so i'm having this problem with the function

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn clamp<T : PartialOrd + PartialEq > ( v : T, lo : T, hi : T) -> T {
     assert!(hi != lo,"{:?} = hi, {:?} = low",hi,lo);
     if v < lo {
         return  lo; 
-     } else if hi < v {
+    } else if hi < v {
         return  hi;
     } else {
         return v;
@@ -15,5 +15,15 @@ fn clamp<T : PartialOrd + PartialEq > ( v : T, lo : T, hi : T) -> T {
 }
 
 fn main () {
+   let  minx = 0.0;
+   let  x=3.4135;
+   let  y=11.423456;
+   let  z=31.3451235;
+    assert_eq!(clamp(x,minx,y),x);
+    assert_eq!(clamp(x,minx,x),false);
+    assert_eq!(clamp(z,minx,y),y);
+    assert_eq!(clamp(y,minx,x),x);
+    assert_eq!(clamp(4.89,0.0,8.9342),4.89);
+    assert_eq!(clamp(9.99,minx,912.1234),9.99);
     println!("{:?}", clamp(4.89,0.0,8.9342));
 }
