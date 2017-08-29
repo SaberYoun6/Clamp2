25fc06c4adc88e6b9603d8037426af8f739cd992
youngse7 <youngse7@mymail.vcu.edu>
2017-08-28 23:52:22 -0400

    I add a debug to the write place

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::fmt::Debug;
+
  // just so i can use clamp in rust 
  // someone started this project and didn't put any work into to it 
  // this will also be the first cargo that i will actaul upload to the cargo page 
  // I need clamp to continue 
-#[cfg(Debug)]
-fn clamp<T : PartialOrd + PartialEq > ( v : T, lo : T, hi : T) -> T {   
+fn clamp<T> ( v : T, lo : T, hi : T) -> T where T: PartialOrd + PartialEq + Debug {   
     assert!(hi != lo,"{:?} = hi, {:?} = low",hi,lo);
     if v < lo {
         return  lo; 
@@ -19,11 +20,10 @@ fn main () {
    let  x=3.4135;
    let  y=11.423456;
    let  z=31.3451235;
-    assert_eq!(clamp(x,minx,y),x);
-    assert_eq!(clamp(x,minx,x),false);
-    assert_eq!(clamp(z,minx,y),y);
-    assert_eq!(clamp(y,minx,x),x);
-    assert_eq!(clamp(4.89,0.0,8.9342),4.89);
-    assert_eq!(clamp(9.99,minx,912.1234),9.99);
-    println!("{:?}", clamp(4.89,0.0,8.9342));
+   assert_eq!(clamp(x,minx,y),x);
+   assert_eq!(clamp(z,minx,y),y);
+   assert_eq!(clamp(y,minx,x),x);
+   assert_eq!(clamp(4.89,0.0,8.9342),4.89);
+   assert_eq!(clamp(9.99,minx,912.1234),9.99);
+   println!("{:?}", clamp(4.89,0.0,8.9342));
 }
